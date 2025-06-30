21a023ecb39bfd20222e605c411399a2be864f62
boooobea <childhop90@gmail.com>
2025-06-30 15:26:28 +0900

    대소문자 바꿔서 출력하기

---
+++ Test/대소문자 바꿔서 출력하기/README.md
@@ -0,0 +1,8 @@
+
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949)
+
+영어 알파벳으로 이루어진 문자열 str이 주어집니다.
+각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+
+aBcDeFg
+AbCdEfG
---
+++ Test/대소문자 바꿔서 출력하기/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char arr [] = sc.nextLine().toCharArray();
+        String str = "";
+        for ( char a : arr ) {
+            if (Character.isUpperCase(a)) {
+                str += String.valueOf(a).toLowerCase();
+            } else {
+                str += String.valueOf(a).toUpperCase();
+            }
+        }
+        System.out.println(str);
+    }
+}
